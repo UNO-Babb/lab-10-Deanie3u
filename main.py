f185aa87ed6b59a03bd6a512022946dfebc4a3d2
Deanie3u <geniet21@gmail.com>
2025-04-16 09:15:57 -0500

    not done

--- main.py
+++ main.py
@@ -2,3 +2,13 @@
 #Name:
 #Date:
 #Assignment:
+
+import coffee
+coffee = coffee.get_coffee()
+
+num_items = item(coffee)
+
+for spot in range(num_items):
+    total_score = coffee[spot]["Data"]["Score"]["Total"]
+
+    print(total_score)
