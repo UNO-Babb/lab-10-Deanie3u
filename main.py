4340eb4edff9c47d322302d32b1ab96432c5f914
Deanie3u <geniet21@gmail.com>
2025-04-16 15:05:35 +0000

    Done

--- main.py
+++ main.py
@@ -3,12 +3,24 @@
 #Date:
 #Assignment:
 
-import coffee
-coffee = coffee.get_coffee()
+import hospitals
+hospitals = hospitals.get_hospitals()
 
-num_items = item(coffee)
+num_items = len(hospitals)
+
+ratings = []
+procedures =[]
 
 for spot in range(num_items):
-    total_score = coffee[spot]["Data"]["Score"]["Total"]
+    rating = hospitals[spot]["Rating"]["Overall"]
+    total_procedure = hospitals[spot]["Procedure"]["Pneumonia"]["Cost"]
+    print(rating, total_procedure)
+    if rating <3:
+        procedures.append(total_procedure)
+        ratings.append(rating)
 
-    print(total_score)
+import matplotlib.pyplot as plt
+plt.plot(ratings, procedures, 'ro')
+plt.plot('Total Procedures')
+plt.ylabel('Rating')
+plt.savefig("output")
